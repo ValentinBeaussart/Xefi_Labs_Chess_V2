87a7830e9c3b3cd347847e286cf69fc97dfce384
Valentin.B <v.beaussart@citronmauve.com>
2022-11-16 17:37:16 +0100

    toto

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,24 @@ class PlayersController < ApplicationController
     @players = Player.all.each do |v|
       v[:point] = v[:victory] - v[:defeat] end.sort_by { |a| a[:point] }.reverse
   end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    @player.update(player_params)
+    redirect_to players_path(@players)
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :position, :victory, :defeat)
+  end
 end
