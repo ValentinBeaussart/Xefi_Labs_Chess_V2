042d09557df4c78168c40fc380c60ba1b82f6e0a
Valentin.B <v.beaussart@citronmauve.com>
2022-11-22 13:50:40 +0100

    chore: transfer the devices concerns method into devices controlleer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :players
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
